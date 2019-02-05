e0c44f2537fff1503ce72474dd79cf3e7b9d8cc3
GabuandLemo-PC\Gabu and Lemo <sac.cabrera@yavirac.edu.ec>
2019-02-05 03:03:35 -0500

    Tablas relacionales

--- src/app/layout/tables/tables.component.ts
+++ src/app/layout/tables/tables.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
+import { EntityService } from 'src/app/providers/entity/entity.service';
+import { AgreementService } from 'src/app/providers/agreement/agreement.service';
+import { PersonService } from 'src/app/providers/person/person.service';
+import { ProjectService } from 'src/app/providers/project/project.service';
 
 @Component({
     selector: 'app-tables',
@@ -8,7 +12,57 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class TablesComponent implements OnInit {
-    constructor() {}
+    constructor
+        (
+            public restEntity: EntityService,
+            public restAgreement: AgreementService,
+            public restPerson: PersonService,
+            public restProject: ProjectService
+        ) {
 
-    ngOnInit() {}
+    }
+
+    projects: any = [];
+    entities: any = [];
+    students: any = [];
+    agreements: any = [];
+
+    ngOnInit() {
+        this.getEntities();
+        this.getProjects();
+        this.getAgreements();
+        this.getStudents();
+    }
+
+    getEntities() {
+        this.entities = [];
+        this.restEntity.getEntities().subscribe((data: {}) => {
+            console.log(data);
+            this.entities = data;
+        });
+    }
+
+    getProjects() {
+        this.projects = [];
+        this.restProject.getProjects().subscribe((data: {}) => {
+            console.log(data);
+            this.projects = data;
+        });
+    }
+
+    getAgreements() {
+        this.agreements = [];
+        this.restAgreement.getAgreements().subscribe((data: {}) => {
+            console.log(data);
+            this.agreements = data;
+        });
+    }
+
+    getStudents() {
+        this.students = [];
+        this.restPerson.getStudents().subscribe((data: {}) => {
+            console.log(data);
+            this.students = data;
+        });
+    }
 }
--- src/app/layout/tables/tables.module.ts
+++ src/app/layout/tables/tables.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TablesRoutingModule } from './tables-routing.module';
 import { TablesComponent } from './tables.component';
 import { PageHeaderModule } from './../../shared';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, TablesRoutingModule, PageHeaderModule],
+    imports: [CommonModule, TablesRoutingModule, PageHeaderModule, FormsModule],
     declarations: [TablesComponent]
 })
 export class TablesModule {}
